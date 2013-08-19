89a03f644c091852f871ff2bd7b438759f7bdf6c
bj <britneyjo.ludkowski@gmail.com>
2013-08-19 17:53:08 +0100

    refactored code with a loop

--- main.rb
+++ main.rb
@@ -7,37 +7,56 @@ def checkout
   change = (cash_given - total_cost)
   puts "The total change needed is #{(change/100)}."
 
-  two_pound_coin = (change/200).to_i
-  puts "#{two_pound_coin} x two pound coin"
-  new_change = (change - (two_pound_coin * 200))
+  possible_coins = [:two_pound_coin, :one_pound_coin, :fifty_p, :twenty_p, :ten_p, :five_p, :two_p, :one_p]
 
-  one_pound_coin = (new_change/ 100).to_i
-  puts "#{one_pound_coin} x one pound coin"
-  new_change = (new_change - (one_pound_coin*100))
+  coin_values = {two_pound_coin: 200, one_pound_coin: 100, fifty_p: 50, twenty_p: 20, ten_p: 10, five_p: 5, two_p: 2, one_p: 1}
 
-  fifty_p = (new_change/50).to_i
-  puts "#{fifty_p} x 50p coin"
-  new_change = (new_change - (fifty_p*50))
+  result ={}
+  available_coins = {two_pound_coin: 5, one_pound_coin: 5, fifty_p: 0, twenty_p: 5, ten_p: 5, five_p: 0, two_p: 5, one_p: 5}
 
-  twenty_p = (new_change/20).to_i
-  puts "#{twenty_p} x 20p coin"
-  new_change = (new_change - (twenty_p*20))
+possible_coins.each do |coin|
+  coins_needed = (change/coin_values[coin]).to_i
+  if available_coins[coin] >= coins_needed
+    result[coin] = coins_needed
+    available_coins[coin] -= coins_needed
+    puts "#{result[coin]} x #{coin}"
+    change = (change - (result[coin] * coin_values[coin]))
+  end
+end
 
-  ten_p = (new_change/10).to_i
-  puts "#{ten_p} x 10p coin"
-  new_change = (new_change - (ten_p*10))
+puts available_coins.inspect
 
-  five_p = (new_change/5).to_i
-  puts "#{five_p} x 5p coin"
-  new_change = (new_change - (five_p*5))
+  # two_pound_coin = (change/200).to_i
+  # puts "#{two_pound_coin} x two pound coin"
+  # new_change = (change - (two_pound_coin * 200))
 
-  two_p = (new_change/2).to_i
-  puts "#{two_p} x 2p coin"
-  new_change = (new_change - (ten_p*2))
+  # one_pound_coin = (new_change/ 100).to_i
+  # puts "#{one_pound_coin} x one pound coin"
+  # new_change = (new_change - (one_pound_coin*100))
 
-  one_p = (new_change/1).to_i
-  puts "#{one_p} x 1p coin"
-  new_change = (new_change - (one_p*1))
+  # fifty_p = (new_change/50).to_i
+  # puts "#{fifty_p} x 50p coin"
+  # new_change = (new_change - (fifty_p*50))
+
+  # twenty_p = (new_change/20).to_i
+  # puts "#{twenty_p} x 20p coin"
+  # new_change = (new_change - (twenty_p*20))
+
+  # ten_p = (new_change/10).to_i
+  # puts "#{ten_p} x 10p coin"
+  # new_change = (new_change - (ten_p*10))
+
+  # five_p = (new_change/5).to_i
+  # puts "#{five_p} x 5p coin"
+  # new_change = (new_change - (five_p*5))
+
+  # two_p = (new_change/2).to_i
+  # puts "#{two_p} x 2p coin"
+  # new_change = (new_change - (two_p*2))
+
+  # one_p = (new_change/1).to_i
+  # puts "#{one_p} x 1p coin"
+  # new_change = (new_change - (one_p*1))
 end
 
 
